26c10e421e95d98c271f9defbb7a4f29142a9899
Ian Philips <iansphilips@gmail.com>
2020-12-13 12:55:15 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1af216b9ea5b0a36fce9.js"
+    "url": "webpack-runtime-abc76727167c764e4d84.js"
   },
   {
     "url": "styles.9b6d5d32fdffdf8815c5.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2bd87ef1b8dc4603e493f4196d3de626"
+    "revision": "36578c5ff168febd5aa44c65e7ba85ac"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
